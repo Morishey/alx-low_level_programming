7ae3f3631f83d91fba063518e07bab7976805988
Morishey <morishey@onmail.com>
2023-08-13 22:36:19 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,5 @@ int main(void)
 char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fwrite(message, sizeof(char), strlen(message), stderr);
 fwrite("\n", sizeof(char), 1, stderr);
-return (0);
+return (1);
 }
