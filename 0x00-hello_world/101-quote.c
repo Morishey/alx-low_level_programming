e9b07cdf3b5304d99b01d4ad90d612d86ed6af6b
Morishey <morishey@onmail.com>
2023-08-13 22:26:33 -0700

    modified script 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
  */
 int main(void)
 {
 char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fwrite(message, sizeof(char), strlen(message), stderr);
+fwrite("\n", sizeof(char), 1, stderr);
 return (0);
 }
