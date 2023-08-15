ca3741221f16c8252c4ceec09ad76d0b1bba85b2
Morishey <morishey@onmail.com>
2023-08-14 17:09:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alpabrts except e and q
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	if (letter != 'q' && letter != 'e')
+		putchar(letter);
+
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
