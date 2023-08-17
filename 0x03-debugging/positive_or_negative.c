6bcd571039e6d5b372c1a1d83d839d5fedefc57f
Morishey <morishey@onmail.com>
2023-08-17 12:41:29 -0700

    modified positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - posotive or negative
- *
- * Return: Always 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	int n;
+int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	return (0);
+i = 0;
+positive_or_negative(i);
+return (0);
 }
