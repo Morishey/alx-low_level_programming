abc10cfe42e7be65caa0042f8bdee9862403ad75
Morishey <morishey@onmail.com>
2023-08-18 02:56:48 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function name
+ * @a: function's formal parameter
+ * @b: function's formal parameter
+ *
+ * Return: 0 indicates success
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
