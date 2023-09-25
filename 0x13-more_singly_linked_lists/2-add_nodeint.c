b6b0527a3cb70992d8ae8a22cb422146209fe68d
Morishey <morishey@onmail.com>
2023-09-25 01:36:19 -0700

    Mandatory 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+	return (NULL);
+	}
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+	free(newNode);
+	return (NULL);
+	}
+	newNode->len = strlen(str);
+	newNode->next = (*head);
+	(*head) = newNode;
+	return (newNode);
+}
