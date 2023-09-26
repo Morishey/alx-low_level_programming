6a20f072f189cedf33ac56ec559dc91756316c84
Morishey <morishey@onmail.com>
2023-09-26 15:54:30 -0700

    Mandatory5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: First arg.
+ *
+ * Return: Void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *freeNode;
+
+	if (head)
+	{
+	while (*head)
+	{
+	freeNode = *head;
+	*head = (*head)->next;
+	free(freeNode);
+	}
+	*head = NULL;
+	}
+}
